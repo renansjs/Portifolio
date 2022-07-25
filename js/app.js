a212a29c2d5c64489e57501091976c5eee1b9278
Renan Olivera <renanoliveirasjs@gmail.com>
2022-07-25 17:08:26 -0300

    Primeiro commit, incionando o projeto e concluindo o menu de navegação versão web

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+
+
+function stickyNavbar() {
+   
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
